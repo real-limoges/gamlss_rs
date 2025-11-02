e92929fa9ef0736054c9244f5fe3d490143bf616
Real Limoges <b.real.limoges@gmail.com>
2025-11-02 07:33:16 -0800

    genesis commit

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+mod error;
+mod fitting;
+mod splines;
+mod types;
+mod families;
+mod terms;
+
+pub use error::GamError;
+pub mod families;
+pub use terms::Term;
+pub use types::*;
+
+use families::Family;
+
+#[derive(Debug)]
+pub struct GeneralizedAdditiveModel {
+    pub coefficients: Coefficients,
+    pub covariance: CovarianceMatrix,
+    terms: Vec<Term>,
+}
+
+impl GeneralizedAdditiveModel {
+    pub fn fit<F: Family> (
+        x: &Matrix,
+        y: &Vector,
+        terms: &[Term],
+        family: &F,
+    ) -> Result<Self, GamError> {
+        Ok(())
+    }
+}
