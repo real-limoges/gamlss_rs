39c8bb0bb47f8e2bd7e93b4aef1b5537aa66d079
Real Limoges <b.real.limoges@gmail.com>
2025-11-02 21:05:47 -0500

    adding argmin behavior for pirls

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,7 @@
 use ndarray::{Array1, Array2};
-use std::ops::{Deref, DerefMut};
+use std::ops::{Add, Deref, DerefMut, Sub};
+use argmin::core::ArgminError;
+use argmin_math::{ArgminAdd, ArgminSub, ArgminDot, ArgminScaledAdd, ArgminScaledSub, ArgminL1Norm};
 
 // ----- Mnemonics because the ndarray names stink
 pub type Vector = Array1<f64>;
@@ -23,7 +25,39 @@ pub struct PenaltyMatrix(pub Matrix);
 pub struct CovarianceMatrix(pub Matrix);
 
 
-// ----- Impls for Newtypes
+// ----- Impls for LogLambdas (I need to do a bunch of them for argmin_math)
+impl Deref for LogLambdas {
+    type Target = Vector;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl DerefMut for LogLambdas {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl ArgminSub<LogLambdas, LogLambdas> for LogLambdas {
+    fn sub(&self, other: &LogLambdas) -> LogLambdas { LogLambdas((&self.0).sub(&other.0)) }
+}
+impl ArgminAdd<LogLambdas, LogLambdas> for LogLambdas {
+    fn add(&self, other: &LogLambdas) -> LogLambdas { LogLambdas((&self.0).add(&other.0)) }
+}
+impl ArgminDot<LogLambdas, f64> for LogLambdas {
+    fn dot(&self, other: &LogLambdas) -> f64 { (&self.0).dot(&other.0) }
+}
+impl ArgminScaledAdd<LogLambdas, f64, LogLambdas> for LogLambdas {
+    fn scaled_add(&self, scalar: &f64, other: &LogLambdas) -> LogLambdas {
+        LogLambdas(&self.0 + (*scalar * &other.0))
+    }
+}
+impl ArgminScaledSub<LogLambdas, f64, LogLambdas> for LogLambdas {
+    fn scaled_sub(&self, scalar: &f64, other: &LogLambdas) -> LogLambdas {
+        LogLambdas(&self.0 - (*scalar * &other.0))
+    }
+}
+// ----- Impls for everything else
 impl Deref for Coefficients {
     type Target = Vector;
     fn deref(&self) -> &Self::Target {
@@ -36,17 +70,7 @@ impl DerefMut for Coefficients {
     }
 }
 
-impl Deref for LogLambdas {
-    type Target = Vector;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-impl DerefMut for LogLambdas {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
+
 
 impl Deref for ModelMatrix {
     type Target = Matrix;
